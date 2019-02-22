b92e11f0ce094b8ca2ea69af79180423adf51869
Soren Hein <soren.hein@gmail.com>
2019-02-22 21:54:55 +0100

    Added script extract info for partial first cars

---
+++ src/first.pl
@@ -0,0 +1,117 @@
+#!perl
+
+use strict;
+use warnings;
+
+# Extract data from sensor??.txt to focus on partial first cars.
+
+for my $file (@ARGV)
+{
+  open my $fi, "<", $file or die "Cannot open $: $!";
+
+  my $fout = "first/$file";
+  open my $fo, ">", $fout or die "Cannot open $: $!";
+
+  my $state = 0;
+  my $eno = 0;
+  my $seg;
+  my $firstFlag;
+  my @lines;
+  while (my $line = <$fi>)
+  {
+    chomp $line;
+    $line =~ s///g;
+
+    if ($line =~ /^File /)
+    {
+      $line =~ /\/([0-9_]+)_001_channel1.dat/;
+      $seg = $1;
+      if ($eno)
+      {
+        dumplines($fo, $eno-1, $seg, \@lines) if $firstFlag;
+      }
+      @lines = ();
+      $eno++;
+      $state = 1;
+      $firstFlag = 0;
+    }
+    elsif ($state == 1 && $line =~ /of PeakMinima$/)
+    {
+      my $dashes = 0;
+      while ($line = <$fi>)
+      {
+        chomp $line;
+        $line =~ s///g;
+        $dashes++ if ($line =~ /^-----/);
+        push @lines, $line . "\n";
+        last if ($dashes == 2);
+      }
+      push @lines, "\n";
+      $state = 2;
+    }
+    elsif ($state == 2 && $line =~ /^FIRSTCAR /)
+    {
+      push @lines, $line . "\n";
+      while ($line = <$fi>)
+      {
+        chomp $line;
+        $line =~ s///g;
+        last if ($line =~ /anywheeler/);
+        push @lines, $line . "\n";
+      }
+      push @lines, "\n";
+      $firstFlag = 1;
+      $state = 3;
+    }
+    elsif ($state == 3 && $line =~ /^Cars after range/)
+    {
+      while ($line = <$fi>)
+      {
+        chomp $line;
+        $line =~ s///g;
+        last if ($line =~ /^All selected/);
+        last if ($line =~ /^Range/);
+        push @lines, $line . "\n";
+      }
+      $state = 4;
+    }
+    elsif ($state == 4 && $line =~ /^True train/)
+    {
+      while ($line = <$fi>)
+      {
+        chomp $line;
+        $line =~ s///g;
+        last if ($line =~ /^File /);
+        push @lines, $line . "\n";
+      }
+      dumplines($fo, $eno-1, $seg, \@lines) if $firstFlag;
+
+      if ($line && $line =~ /^File /)
+      {
+        $line =~ /\/([0-9_]+)_001_channel1.dat/;
+        $seg = $1;
+        @lines = ();
+        $eno++;
+        $state = 1;
+        $firstFlag = 0;
+      }
+    }
+  }
+
+  close $fi;
+  close $fo;
+}
+
+exit;
+
+
+sub dumplines
+{
+  my ($fo, $no, $seg, $lines_ref) = @_;
+  print $fo "-" x 72 . "\n";
+  print $fo "$seg, number $no\n\n";
+  for my $line (@$lines_ref)
+  {
+    print $fo $line;
+  }
+}
